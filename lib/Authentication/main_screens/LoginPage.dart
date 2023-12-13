28000b65c4371c7aade958e505839fdfa2453896
harvey0911 <A.AitMansour@aui.ma>
2023-12-13 15:07:41 +0100

    authenticatio trials, latest

--- lib/Authentication/main_screens/LoginPage.dart
+++ lib/Authentication/main_screens/LoginPage.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../../Users/Userc.dart';
 import '../../databases/database_manager.dart';
-import '../../Authentication/results_screen/GoogleDone.dart';
+import '../../pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   static const String id = 'login_page';
@@ -21,42 +20,14 @@ class _LoginPageState extends State<LoginPage> {
   bool _wrongPassword = false;
   String emailText = 'Email doesn\'t match';
   String passwordText = 'Password doesn\'t match';
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<User?> _handleSignIn() async {
-    bool isSignedIn = await _googleSignIn.isSignedIn();
-    if (isSignedIn) {
-      return _auth.currentUser;
-    } else {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
-      return (await _auth.signInWithCredential(credential)).user;
-    }
-  }
-
-  void onGoogleSignIn(BuildContext context) async {
-    setState(() {
-      _showSpinner = true;
-    });
-
-    User? user = await _handleSignIn();
-
-    if (user != null) {
-      Navigator.pushNamed(context, GoogleDone.id, arguments: user);
-    } else {
-      // Handle null user scenario
-    }
-
-    setState(() {
-      _showSpinner = false;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
+
+    Userc user = Userc(fullName: "aymane", email:"aymaneaitmansour@gmail.com", password: "aymane", role: Role.student);
+    DatabaseManager().insertUser(user);
+
+
     return Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
@@ -66,7 +37,7 @@ class _LoginPageState extends State<LoginPage> {
         child: Padding(
           padding: EdgeInsets.only(
               top: 60.0, bottom: 20.0, left: 20.0, right: 20.0),
-          child: SingleChildScrollView( // Make the layout scrollable
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -123,14 +94,11 @@ class _LoginPageState extends State<LoginPage> {
                       _wrongPassword = false;
                     });
                     try {
-
                       bool isAuthenticated = await DatabaseManager().authenticateUser(email!, password!);
 
                       if (isAuthenticated) {
-
                         Navigator.pushReplacementNamed(context, HomePage.id);
                       } else {
-                        // Authentication failed
                         setState(() {
                           _showSpinner = false;
                           _wrongEmail = true;
@@ -139,8 +107,6 @@ class _LoginPageState extends State<LoginPage> {
                           passwordText = 'Invalid credentials';
                         });
                       }
-
-
                     } catch (e) {
                       setState(() {
                         _showSpinner = false;
@@ -159,8 +125,6 @@ class _LoginPageState extends State<LoginPage> {
                     style: TextStyle(fontSize: 25.0, color: Colors.white),
                   ),
                 ),
-                // ... Rest of the widgets like Google and Facebook sign-in buttons
-                // ...
               ],
             ),
           ),
